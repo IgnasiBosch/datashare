ae5f13d1b09cab6827861192165f40a3231b4837
Iggy <me@ignasibosch.com>
2023-12-29 19:08:15 +0100

    Main template

--- server.go
+++ server.go
@@ -3,18 +3,35 @@ package main
 import (
 	"dataShare/db"
 	"dataShare/document"
+	"errors"
 	"fmt"
-	"github.com/labstack/echo/v4/middleware"
-	"gorm.io/gorm"
-	"io"
-	"net/http"
-	"strconv"
-
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"gorm.io/gorm"
+	"html/template"
+	"io"
 	"log"
+	"net/http"
 	"os"
+	"strconv"
 )
+
+type Template struct {
+	//templates *template.Template
+	templates map[string]*template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	//return t.templates.ExecuteTemplate(w, name, data)
+
+	tmpl, ok := t.templates[name]
+	if !ok {
+		err := errors.New("Template not found -> " + name)
+		return err
+	}
+	return tmpl.ExecuteTemplate(w, "base.html", data)
+}
 
 func upload(c echo.Context) error {
 	// Read form fields
@@ -57,6 +74,16 @@ func upload(c echo.Context) error {
 	return c.HTML(http.StatusOK, fmt.Sprintf("<p>Uploaded successfully %d files with fields name=%s and email=%s.</p>", len(files), name, email))
 }
 
+func indexHandler(c echo.Context) error {
+
+	return c.Render(http.StatusOK, "home.html", map[string]interface{}{
+		"name": "About",
+		"msg":  "All about Boatswain!",
+		"csrf": c.Get("csrf"),
+	})
+
+}
+
 // main is the entry point of the application.
 // It initializes the environment variables, establishes a database connection, performs database migration,
 // creates a new Echo instance, defines a route to handle the root path, and starts the server.
@@ -72,24 +99,24 @@ func main() {
 	dbMigrate(dbConn)
 
 	e := echo.New()
+
+	templates := make(map[string]*template.Template)
+	templates["home.html"] = template.Must(template.ParseFiles("view/home.html", "view/base.html"))
+	e.Renderer = &Template{
+		templates: templates,
+	}
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+	e.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
+		TokenLookup: "form:csrf",
+	}))
 
-	e.Static("/", "public")
+	e.Static("/static", "static")
+
+	e.GET("/", indexHandler)
 	e.POST("/upload", upload)
 
 	e.Logger.Fatal(e.Start("localhost:" + appPort))
-
-	//e.GET("/", func(c echo.Context) error {
-	//	return c.String(http.StatusOK, "Hello, World!")
-	//})
-
-	//// TODO: This middleware is for debug purpose only. Revisit this later.
-	//e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
-	//	Format: "method=${method}, uri=${uri}, status=${status}\n",
-	//}))
-	//e.Logger.Fatal(e.Start("localhost:" + appPort))
-
 }
 
 // getDatabaseConnection returns the database connection object.
